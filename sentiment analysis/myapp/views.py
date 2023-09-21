fab455f275c492c86b99d6be678e68dc9b94478a
prathibha-MH <143241716+prathibha-MH@users.noreply.github.com>
2023-09-21 22:30:35 +0530

    Social Media Sentiment Analysis project
    #MACHINE LEARNING

---
+++ sentiment analysis/myapp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bootstrap(request):
+    return render(request,'bootstrap.html')
+def smsa(request):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    from tensorflow.keras.preprocessing.text import Tokenizer
+    from tensorflow.keras.preprocessing.sequence import pad_sequences
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import LSTM,Dense, Dropout, SpatialDropout1D
+    from tensorflow.keras.layers import Embedding
+
+    df = pd.read_csv("C:\\Users\\Whynew\\OneDrive\\Desktop\\pratibha\\Tweets.csv")
+
+    tweet_df = df[['text','airline_sentiment']]
+    tweet_df = tweet_df[tweet_df['airline_sentiment'] != 'neutral']
+    sentiment_label = tweet_df.airline_sentiment.factorize()
+
+    tweet = tweet_df.text.values
+    tokenizer = Tokenizer(num_words=5000)
+    tokenizer.fit_on_texts(tweet)
+    vocab_size = len(tokenizer.word_index) + 1
+    encoded_docs = tokenizer.texts_to_sequences(tweet)
+    padded_sequence = pad_sequences(encoded_docs, maxlen=200)
+
+    embedding_vector_length = 32
+    model = Sequential() 
+    model.add(Embedding(vocab_size, embedding_vector_length, input_length=200) )
+    model.add(SpatialDropout1D(0.25))
+    model.add(LSTM(50, dropout=0.5, recurrent_dropout=0.5))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='sigmoid')) 
+    model.compile(loss='binary_crossentropy',optimizer='adam', metrics=['accuracy'])  
+
+    model.fit(padded_sequence,sentiment_label[0],validation_split=0.2, epochs=3, batch_size=32)
+    if(request.method=="POST"):
+        data=request.POST
+        Text=data.get('txtcmt')
+        if('submit' in request.POST):
+            tw = tokenizer.texts_to_sequences([Text])
+            tw = pad_sequences(tw,maxlen=200)
+            prediction = int(model.predict(tw).round().item())
+            output=sentiment_label[1][prediction]
+            return render(request,"smsa.html",context={'Text':Text,'output':output})
+    return render(request,"smsa.html")
